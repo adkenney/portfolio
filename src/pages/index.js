a80e0e3f12873fc4a76c70a82c9c207d8a9a1099
Arkell Kenney <kell@Arkells-MacBook-Air.local>
2023-05-17 14:01:37 -0400

    add contact to main page, icons, and resume link

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Hero from '@/components/Hero/Hero';
 import About from '@/components/About/About';
 import Skills from '@/components/Skills/Skills';
 import Projects from '@/components/Projects/Projects';
+import Contact from '@/components/Contact/Contact';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </>
   );
 }
