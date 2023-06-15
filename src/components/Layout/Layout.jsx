b4e563e19210ba8ed969ef1b6d581fc5c820a750
Arkell Kenney <kell@Arkells-MacBook-Air.local>
2023-06-15 15:34:08 -0400

    fix layout alignment on bigger screens

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,7 @@
 export default function Layout({ children }) {
-  return <div className="p-5 md:px-12 lg:px-36 max-w-[1440px]">{children}</div>;
+  return (
+    <div className="p-5 md:px-12 lg:px-36 max-w-[1440px] mx-auto">
+      {children}
+    </div>
+  );
 }
